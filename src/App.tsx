c3a58e832993d896bb9e23d2923313028c3f53ae
yamaguchi1113 <23238474@edu.cc.saga-u.ac.jp>
2025-04-10 10:25:14 +0900

    sassの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-     
+     <h1>konnichiwa</h1>
     </div>
   );
 }
