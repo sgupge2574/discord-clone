67ba778aa90f0907c91d7f5a09576dd3c0b88888
yamaguchi1113 <23238474@edu.cc.saga-u.ac.jp>
2025-04-13 03:22:57 +0900

    sidebarのフロント完成

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,38 +1,63 @@
-import React from 'react'
-import './Sidebar.scss'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import AddIcon from '@mui/icons-material/Add';
+import React from "react";
+import SidebarChannel from "./SidebarChannel";
+import "./Sidebar.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadsetMicIcon from "@mui/icons-material/HeadsetMic";
+import SettingsIcon from "@mui/icons-material/Settings";
+
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       {/* sidebarLeft */}
-      <div className='sidebarLeft'>
-        <div className='serverIcon'>
+      <div className="sidebarLeft">
+        <div className="serverIcon">
           <img src="./logo192.png" alt="" />
         </div>
-        <div className='serverIcon'>
+        <div className="serverIcon">
           <img src="./logo512.png" alt="" />
         </div>
       </div>
       {/* sidebarRight */}
-      <div className='sidebarRight'>
-        <div className='sidebarTop'>
+      <div className="sidebarRight">
+        <div className="sidebarTop">
           <h3>Discord</h3>
           <ExpandMoreIcon />
         </div>
         {/* sidebarChannels */}
-        <div className='sidebarChannels'>
-          <div className='sidebarChannelsHedder'>
-            <div className='sidebarHedder'>
-            <ExpandMoreIcon />
-            <h4>プログラミングチャンネル</h4>
+        <div className="sidebarChannels">
+          <div className="sidebarChannelsHedder">
+            <div className="sidebarHedder">
+              <ExpandMoreIcon />
+              <h4>チャンネル</h4>
             </div>
-            <AddIcon className='sidebarAddIcon'/>
+            <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+          </div>
+        </div>
+        <div className="sidebarFooter">
+          <div className="sidebarAccount">
+            <img src="./iconme.png" alt="" />
+            <div className="accountName">
+              <h4>命優</h4>
+              <span>#8162</span>
+            </div>
+          </div>
+          <div className="sidebarVoice">
+            <MicIcon />
+            <HeadsetMicIcon />
+            <SettingsIcon />
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
